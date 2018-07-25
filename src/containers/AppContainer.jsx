2f9652fc0d5e56a65eade56cf4dad6c816db76f2
mrmgreen <m.r.green@hotmail.co.uk>
2018-07-25 18:04:56 +0100

    tidy up of container

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -2,22 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import App from '../components/App';
 
-class AppContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <App title="boom shake the room" quotes={this.props.quotes} />
-    )
-  }
-}
-
 const mapStateToProps = (state) => {
   return {
     quotes: state.quotes,
   }
 }
 
-export default connect(mapStateToProps)(AppContainer)
+const AppContainer = connect(mapStateToProps)(App)
+
+export default AppContainer
