9549543b6f90ec7921902c34351cb348ab46215f
mrmgreen <m.r.green@hotmail.co.uk>
2018-07-30 08:15:02 +0100

    creating a redux thunk

--- test/actions/index.spec.js
+++ test/actions/index.spec.js
@@ -1,8 +1,34 @@
-import { fetchRandomAlanQuote } from '../../src/actions';
+import { fetchRandomAlanQuote, REQUEST_RANDOM_ALAN_QUOTE, RECEIVED_RANDOM_ALAN_QUOTE } from '../../src/actions';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 describe('fetchRandomAlanQuote', () => {
   it('should return a function', () => {
     expect(fetchRandomAlanQuote()).to.be.a('function');
-  })
-})
+  });
+
+  it('should dispatch requestRandomAlanQuote and receivedRandomAlanQuote', () => {
+    const jsonValue = JSON.stringify("hello");
+    const jsonStub = sinon.stub().returns(jsonValue)
+    const response = { json: jsonStub };
+    const fetchStub = sinon.stub().resolves(response);
+    global.fetch = fetchStub;
+    const dispatchStub = sinon.stub();
+    const thunk = fetchRandomAlanQuote();
+    const action = thunk(dispatchStub);
+
+    const requestRandomAlanQuoteAction = { type: REQUEST_RANDOM_ALAN_QUOTE };
+    const receivedRandomAlanQuoteAction =
+    {
+      type: RECEIVED_RANDOM_ALAN_QUOTE,
+      randomAlanQuote: JSON.parse(jsonValue),
+    };
+
+    expect(dispatchStub.calledWith(requestRandomAlanQuoteAction)).to.equal(true);
+
+    action.then(() => {
+      expect(dispatchStub.calledTwice).to.equal(true);
+      expect(dispatchStub.calledWith(receivedRandomAlanQuoteAction)).to.equal(true);
+    }).catch(er => expect(er).to.equal(false))
+  });
+});
